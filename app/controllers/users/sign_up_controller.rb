228b1cf62030d73bcd58920f6f66fb146d6beb49
Gustavo Duarte <gustavo_dmuniz@hotmail.com>
2021-08-28 20:37:39 -0300

    add password in params in sign_up_controller

--- app/controllers/users/sign_up_controller.rb
+++ app/controllers/users/sign_up_controller.rb
@@ -1,24 +1,23 @@
-class Users::SignUpController < ApplicationController
-  def create
-    user = User.new(user_params)
-    
-    if user.save
-      payload = { user_id: user.id }
-      session = JWTSessions::Session.new(payload: payload, refresh_by_access_allowed: true)
-      tokens = session.login
-      response.set_cookie(JWTSessions.access_cookie,
-                          value: tokens[:access],
-                          httponly: true,
-                          secure: Rails.env.production?)
+# frozen_string_literal: true
 
-      render json: { csrf: tokens[:csrf] }
-    else
-      render json: { error: user.errors.full_messages.join(' ,') }, status: :unprocessable_entity
+module Users
+  class SignUpController < ApplicationController
+    def create
+      user = User.new(user_params)
+
+      if user.save
+        payload = { user_id: user.id }
+        session = JWTSessions::Session.new(payload: payload)
+        render json: { jwt: session.login[:access] }
+      else
+        render json: { error: user.errors.full_messages.join(' ,') }, status: :unprocessable_entity
+      end
     end
-  end
-  private 
 
-  def user_params
-    params.permit(:uid, :name, :image, :email, :token, :expires_at, :refresh_token, :expires)
+    private
+
+    def user_params
+      params.permit(:name, :email, :password, :password_confirmation)
+    end
   end
 end
--- spec/requests/users/sign_up_request_spec.rb
+++ spec/requests/users/sign_up_request_spec.rb
@@ -1,13 +1,46 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "Users::SignUps", type: :request do
+RSpec.describe 'Users::SignUps', type: :request do
+  describe 'User Sign Up' do
+    let(:password) { Faker::Internet.password }
+    let(:password_confirmation) { password }
+    let(:email) { Faker::Internet.email }
+    let(:user) do
+      {
+        email: email,
+        name: Faker::Commerce.product_name,
+        password: password,
+        password_confirmation: password_confirmation
+      }
+    end
 
-  describe "User Sign Up" do
-    it "returns http success" do
-      post "/users/sign_up", params: build(:user).attributes.slice('email', 'name', 'uid')
-      expect(response).to have_http_status(:success)
-      expect(cookies[:jwt_access]).to_not eq nil
+    subject { post '/users/sign_up', params: user }
+
+    context 'when fill correctly user data ' do
+      it 'returns http success' do
+        subject
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context 'when not correctly filling in the user data' do
+      context 'and send email password ' do
+        let(:email) { Faker::Alphanumeric.alphanumeric(number: 10) }
+        it 'returns http unprocessable entity' do
+          subject
+          expect(response).to have_http_status(:unprocessable_entity)
+        end
+      end
+
+      context 'and send invalid password confirmation' do
+        let(:password_confirmation) { '' }
+        it 'returns http unprocessable entity' do
+          subject
+          expect(response).to have_http_status(:unprocessable_entity)
+        end
+      end
     end
   end
-
 end
