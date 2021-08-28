a11672261b769e4d364c853ef529eec5767b88a0
Gustavo Duarte <gustavo_dmuniz@hotmail.com>
2021-08-28 14:33:14 -0300

    remove aplication service

--- app/services/recurring_operation_manager/recurring_operation_creator.rb
+++ app/services/recurring_operation_manager/recurring_operation_creator.rb
@@ -1,29 +1,33 @@
-module RecurringOperationManager
-  class RecurringOperationCreator < ApplicationService
-    attr_accessor :recurring_operation
+# frozen_string_literal: true
 
+module RecurringOperationManager
+  class RecurringOperationCreator
     def initialize(recurring_operation)
       @recurring_operation = recurring_operation
+      @user = recurring_operation.user
+      @ends = @recurring_operation.ends_in || Time.now
     end
 
     def call
       due_date = @recurring_operation.starts_in.beginning_of_month
-      ends = @recurring_operation.ends_in || Time.now
-      user = @recurring_operation.user
-      while due_date <= ends
-        @recurring_operation.monthly_operations.new(
-          name: @recurring_operation.name,
-          value: @recurring_operation.value,
-          date_of_operation: calculate_due_date(due_date),
-          operation_type: @recurring_operation.operation_type,
-          user: user
-        )
+      while due_date <= @ends
+        add_recurring_operation(due_date)
         due_date += 1.month
       end
       @recurring_operation.save
     end
 
     private
+
+    def add_recurring_operation(due_date)
+      @recurring_operation.monthly_operations.new(
+        name: @recurring_operation.name,
+        value: @recurring_operation.value,
+        date_of_operation: calculate_due_date(due_date),
+        operation_type: @recurring_operation.operation_type,
+        user: @user
+      )
+    end
 
     def calculate_due_date(due_date)
       temp_date = due_date.change({ day: @recurring_operation.starts_in.day })
