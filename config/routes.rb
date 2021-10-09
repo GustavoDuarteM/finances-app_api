d68de89cd3bbf8cc44b878a3495f2dd23f9bc33c
Gustavo Duarte <gustavo_dmuniz@hotmail.com>
2021-10-09 14:00:58 -0300

    add index in operation controller and rfact tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  
+
   namespace :users do
     resources :sign_up, only: [:create]
     resources :sign_in, only: [:create]
@@ -8,8 +8,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :operations, only: [:create, :show, :destroy, :update]
-      resources :recurring_operations, only: [:create, :show, :destroy, :update]
+      resources :operations, only: %i[index create show destroy update]
+      resources :recurring_operations, only: %i[create show destroy update]
     end
   end
 end
--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -7,7 +7,7 @@ module ControllerMacros
       payload = { user_id: @user.id }
       session = JWTSessions::Session.new(payload: payload, refresh_by_access_allowed: true)
       access_tokens = session.login[:access]
-      @token = { 'Authorization': "Bearer #{access_tokens}" }
+      request.headers['Authorization'] = "Bearer #{access_tokens}"
     end
   end
 end
